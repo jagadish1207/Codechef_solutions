49791f73ebcd05e95aa0d1bf30712be1b9fcb551
jagadish07 <jagadishjoe07@gmail.com>
2021-05-11 09:55:17 +0530

    TicTacToe changes

--- codechefMay4.py
+++ codechefMay4.py
@@ -12,10 +12,10 @@ try:
         return a, b
 
     def check_for_ele_stat(a, b, l):
-            # check row  and column
-            # 11 -> 01 and 21 , 10 and 12
-            # 00 -> 11 and 33
-            # 03 -> 11 and 20
+        # check row  and column
+        # 11 -> 01 and 21 , 10 and 12
+        # 00 -> 11 and 33
+        # 03 -> 11 and 20
         count = 0
         p = l[a][b]
         if (l[(a+1) % 3][b] == p and l[(a+2) % 3][b] == p):
@@ -31,6 +31,26 @@ try:
         if (a == 2 and b == 0):
             if (l[1][1] == p and l[0][2] == p):
                 count+=1
+        return count
+
+    def new_check(l):
+        count = 0
+        if (l[0][0] != "_" and l[0][0] == l[0][1] and l[0][1] == l[0][2]):
+            count+=1
+        if (l[1][0] != "_" and l[1][0] == l[1][1] and l[1][1] == l[1][2]):
+            count+=1
+        if (l[2][0] != "_" and l[2][0] == l[2][1] and l[2][1] == l[2][2]):
+            count+=1
+        if (l[0][0] != "_" and l[0][0] == l[1][0] and l[1][0] == l[2][0]):
+            count+=1
+        if (l[0][1] != "_" and l[0][1] == l[1][1] and l[1][1] == l[2][1]):
+            count+=1
+        if (l[0][2] != "_" and l[0][2] == l[1][2] and l[1][2] == l[2][2]):
+            count+=1
+        if (l[0][0] != "_" and l[0][0] == l[1][1] and l[1][1] == l[2][2]):
+            count+=1
+        if (l[2][0] != "_" and l[2][0] == l[1][1] and l[1][1] == l[0][2]):
+            count+=1
         return count
 
     def final_check(l):
@@ -70,7 +90,8 @@ try:
         if (b > a or a-b > 1):
             print("3")
         else:
-            c = final_check(l)
+           # c = final_check(l)
+            c = new_check(l)
             if (c==1):
                 print("1")
             elif (c==0):
